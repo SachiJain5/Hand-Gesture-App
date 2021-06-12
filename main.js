a18b9c6bc700369f2cd1418f0e226009a94f3337
SachiJain5 <73456720+SachiJain5@users.noreply.github.com>
2021-06-12 11:16:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 Webcam.set({
     width:350,
     height:300,
-    image_format:'png';
+    image_format:'png',
     png_quality:100
 });
 
@@ -20,5 +20,38 @@ console.log("ml5.version", ml5.version);
 classifier = ml5.imageClassifier = ("https://teachablemachine.withgoogle.com/models/SwFWkHmGH/model.json", modelLoaded);
 
 function modelLoaded(){
-    console.log('Model Loaded!');
+  console.log('Model Loaded!');
 };
+
+function speak(){
+    var synth = window.speechSynthesis;
+    statement1 = "This is " + answer;
+    var utterThis = new SpeechSynthesisUtterance(statement1);
+    synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById("picture_captured");
+    classifier.classify(img, gotResult)
+}
+
+function gotResult(error, results){
+    if(error){
+    console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label
+        answer = results[0].label;
+        speak()
+    }
+    if (results[0] == "Victory"){
+        document.getElementById("updated_emoji").innerHTML = "&#9996;";
+    }
+    if(results[0] == "Best"){
+        document.getElementById("updated_emoji").innerHTML = "&#128076;";
+    }
+    if (results[0] == "Amazing"){
+        document.getElementById("updated_emoji").innerHTML = "&#128077";
+    }
+}
